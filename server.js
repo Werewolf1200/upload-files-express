3d53a059aacb2d41341765fbdd36b06f967c90a3
Saúl Camacho <electrohead1200@gmail.com>
2021-06-10 22:10:49 -0500

    Configurar multer para subir los archivos

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express"); // Guardar en una variable
 const aplicacion = express(); // Convierte la variable anterior en función
+const multer = require('multer');
 
-aplicacion.get("/", (peticion, resultado) => {
-    resultado.send("Hola Mundo");
-}); // dirección, funcion a ejecutar
+const upload = multer({
+    dest: 'uploads/' // Destino donde guardara los archivos
+})
+
+aplicacion.get("/", (peticion, resultado) => {  // dirección, funcion a ejecutar
+    //console.log(__dirname);
+    resultado.sendFile(__dirname + "/views/index.html"); // Lee la ruta del archivo
+}); 
+
+aplicacion.post("/files", upload.single('avatar') ,(peticion, resultado) => {
+    resultado.send("Subido Correctamente");
+});
 
 
 aplicacion.listen(8989, () => console.log("Server Started"));
